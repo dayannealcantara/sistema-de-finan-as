716a902ce7fd063d08d7df3a4567dc0be1b0d8d8
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-12 09:34:12 -0300

    feat:importing components , styling main page

---
+++ src/App.styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div``
+export const Header = styled.header`
+  background-color:darkblue;
+  text-align:center;
+  height:150px;
+`
+export const HeaderText = styled.h1`
+  margin:0;
+  padding:0;
+  color:#fff;
+  padding-top:30px;
+`
+export const Body = styled.div`
+  margin:auto;
+  max-width:980px;
+  background-color:pink;
+  margin-bottom:50px;
+`
