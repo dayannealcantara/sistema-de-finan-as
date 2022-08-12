38071b97de28e313355ee454664dac420d4d30c0
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-12 09:20:11 -0300

    feat:adding the initial data (mocks)

---
+++ src/data/item.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[]=[
+  {date: new Date(2022, 6, 10), category: 'food', title:'McDonalds', value: 32.12},
+  {date: new Date(2022, 6, 9), category: 'food', title:'McDonalds', value: 28},
+  {date: new Date(2022, 6, 2), category: 'rent', title:'Aluguel apt', value: 2000},
+  {date: new Date(2022, 7, 10), category: 'salary', title:'Salário', value: 4500}
+]
